d978d226d32148c7aa1ceb6eb21f13a176d93faa
montserratPaus <montserratpaus@gmail.com>
2021-12-01 18:57:05 +0100

    actualizacion PK y FK

--- bbdd/dragonesYmazmorras.sql
+++ bbdd/dragonesYmazmorras.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4deb2ubuntu5
+-- version 4.9.5deb2
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 01-12-2021 a las 16:34:40
--- Versión del servidor: 8.0.27-0ubuntu0.21.10.1
--- Versión de PHP: 8.0.8
+-- Tiempo de generación: 01-12-2021 a las 17:56:23
+-- Versión del servidor: 8.0.27-0ubuntu0.20.04.1
+-- Versión de PHP: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -31,7 +32,7 @@ USE `dragonesYmazmorras`;
 
 DROP TABLE IF EXISTS `armadurasYEscudos`;
 CREATE TABLE IF NOT EXISTS `armadurasYEscudos` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `tipo` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `clase_de_armadura` int NOT NULL,
@@ -49,13 +50,13 @@ CREATE TABLE IF NOT EXISTS `armadurasYEscudos` (
 
 DROP TABLE IF EXISTS `armas`;
 CREATE TABLE IF NOT EXISTS `armas` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `alcance` int NOT NULL,
   `a_dos_manos` tinyint(1) NOT NULL,
   `arrojadiza` tinyint(1) NOT NULL,
   `cargar` tinyint(1) NOT NULL,
-  `distancia` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `distancia` int NOT NULL,
   `especial` tinyint(1) NOT NULL,
   `municion` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `pesado` tinyint(1) NOT NULL,
@@ -75,7 +76,7 @@ CREATE TABLE IF NOT EXISTS `armas` (
 
 DROP TABLE IF EXISTS `clases`;
 CREATE TABLE IF NOT EXISTS `clases` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `dado_de_golpe` int NOT NULL,
   `competencias_elegir` json NOT NULL,
@@ -97,7 +98,8 @@ CREATE TABLE IF NOT EXISTS `competenciasClase` (
   `id_clase` int NOT NULL,
   `tipo` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `competencia` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `experto` tinyint(1) NOT NULL
+  `experto` tinyint(1) NOT NULL,
+  KEY `fk_competenciasClase-clase` (`id_clase`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -111,7 +113,8 @@ CREATE TABLE IF NOT EXISTS `competenciasPersonaje` (
   `id_personaje` int NOT NULL,
   `tipo` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `competencia` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `experto` tinyint(1) NOT NULL
+  `experto` tinyint(1) NOT NULL,
+  KEY `fk_competenciasPersonaje-personaje` (`id_personaje`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -125,7 +128,8 @@ CREATE TABLE IF NOT EXISTS `competenciasRaza` (
   `id_raza` int NOT NULL,
   `tipo` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `competencia` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `experto` tinyint(1) NOT NULL
+  `experto` tinyint(1) NOT NULL,
+  KEY `fk_competenciasRaza-raza` (`id_raza`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -139,7 +143,8 @@ CREATE TABLE IF NOT EXISTS `competenciasTrasfondo` (
   `id_trasfondo` int NOT NULL,
   `tipo` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `competencia` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `experto` tinyint(1) NOT NULL
+  `experto` tinyint(1) NOT NULL,
+  KEY `fk_competenciasTrasfondo-trasfondo` (`id_trasfondo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -150,13 +155,13 @@ CREATE TABLE IF NOT EXISTS `competenciasTrasfondo` (
 
 DROP TABLE IF EXISTS `conjuros`;
 CREATE TABLE IF NOT EXISTS `conjuros` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nombre` int NOT NULL,
   `nivel` int NOT NULL,
   `tipo` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `tiempo_de_lanzamiento` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `alcance` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `componentes` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `componentes` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `duracion` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `descripcion` varchar(2000) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   PRIMARY KEY (`id`)
@@ -171,7 +176,9 @@ CREATE TABLE IF NOT EXISTS `conjuros` (
 DROP TABLE IF EXISTS `conjurosClase`;
 CREATE TABLE IF NOT EXISTS `conjurosClase` (
   `id_clase` int NOT NULL,
-  `id_conjuro` int NOT NULL
+  `id_conjuro` int NOT NULL,
+  KEY `fk_conjurosClase-clase` (`id_clase`),
+  KEY `fk_conjurosClase-conjuro` (`id_conjuro`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -184,7 +191,9 @@ DROP TABLE IF EXISTS `conjurosPersonaje`;
 CREATE TABLE IF NOT EXISTS `conjurosPersonaje` (
   `id_personaje` int NOT NULL,
   `id_conjuro` int NOT NULL,
-  `preparado` tinyint(1) NOT NULL
+  `preparado` tinyint(1) NOT NULL,
+  KEY `fk_conjurosPersonaje-conjuro` (`id_conjuro`),
+  KEY `fk_conjurosPersonaje-personaje` (`id_personaje`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -195,12 +204,12 @@ CREATE TABLE IF NOT EXISTS `conjurosPersonaje` (
 
 DROP TABLE IF EXISTS `idiomas`;
 CREATE TABLE IF NOT EXISTS `idiomas` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `descripcion` varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `escritura` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `idiomas`
@@ -224,7 +233,9 @@ INSERT INTO `idiomas` VALUES(8, 'Orco', 'El Orco es un idioma estridente y áspe
 DROP TABLE IF EXISTS `idiomasClase`;
 CREATE TABLE IF NOT EXISTS `idiomasClase` (
   `id_idioma` int NOT NULL,
-  `id_clase` int NOT NULL
+  `id_clase` int NOT NULL,
+  KEY `fk_idiomasClase-idioma` (`id_idioma`),
+  KEY `fk_idiomasClase-clase` (`id_clase`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -236,7 +247,9 @@ CREATE TABLE IF NOT EXISTS `idiomasClase` (
 DROP TABLE IF EXISTS `idiomasPersonaje`;
 CREATE TABLE IF NOT EXISTS `idiomasPersonaje` (
   `id_idioma` int NOT NULL,
-  `id_personaje` int NOT NULL
+  `id_personaje` int NOT NULL,
+  KEY `fk_idiomasPersonaje-idioma` (`id_idioma`),
+  KEY `fk_idiomasPersonaje-personaje` (`id_personaje`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -248,7 +261,9 @@ CREATE TABLE IF NOT EXISTS `idiomasPersonaje` (
 DROP TABLE IF EXISTS `idiomasRaza`;
 CREATE TABLE IF NOT EXISTS `idiomasRaza` (
   `id_idioma` int NOT NULL,
-  `id_raza` int NOT NULL
+  `id_raza` int NOT NULL,
+  KEY `fk_idiomaRaza-idioma` (`id_idioma`),
+  KEY `fk_idiomaRaza-raza` (`id_raza`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -259,7 +274,7 @@ CREATE TABLE IF NOT EXISTS `idiomasRaza` (
 
 DROP TABLE IF EXISTS `objetos`;
 CREATE TABLE IF NOT EXISTS `objetos` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `descripcion` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `peso` int NOT NULL,
@@ -274,7 +289,7 @@ CREATE TABLE IF NOT EXISTS `objetos` (
 
 DROP TABLE IF EXISTS `personajes`;
 CREATE TABLE IF NOT EXISTS `personajes` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `jugador` int NOT NULL,
   `raza` int NOT NULL,
@@ -318,7 +333,11 @@ CREATE TABLE IF NOT EXISTS `personajes` (
   `habilidad_de_conjuros` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `salvacion_conjuro` int NOT NULL,
   `bonus_ataque_conjuro` int NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `fk_personajes-clase` (`clase`),
+  KEY `fk_personajes-jugador` (`jugador`),
+  KEY `fk_personajes-raza` (`raza`),
+  KEY `fk_personajes-trasfondo` (`trasfondo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -330,7 +349,9 @@ CREATE TABLE IF NOT EXISTS `personajes` (
 DROP TABLE IF EXISTS `personajeUsuario`;
 CREATE TABLE IF NOT EXISTS `personajeUsuario` (
   `id_usuario` int NOT NULL,
-  `id_personaje` int NOT NULL
+  `id_personaje` int NOT NULL,
+  KEY `fk_personajeUsuario-personaje` (`id_personaje`),
+  KEY `fk_personajeUsuario-usuario` (`id_usuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -341,7 +362,7 @@ CREATE TABLE IF NOT EXISTS `personajeUsuario` (
 
 DROP TABLE IF EXISTS `rasgos`;
 CREATE TABLE IF NOT EXISTS `rasgos` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `descripcion` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `activo` tinyint(1) NOT NULL,
@@ -357,7 +378,9 @@ CREATE TABLE IF NOT EXISTS `rasgos` (
 DROP TABLE IF EXISTS `rasgosClase`;
 CREATE TABLE IF NOT EXISTS `rasgosClase` (
   `id_rasgo` int NOT NULL,
-  `id_clase` int NOT NULL
+  `id_clase` int NOT NULL,
+  KEY `fk_rasgosClase-clase` (`id_clase`),
+  KEY `fk_rasgosClase-rasgo` (`id_rasgo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -369,7 +392,9 @@ CREATE TABLE IF NOT EXISTS `rasgosClase` (
 DROP TABLE IF EXISTS `rasgosRaza`;
 CREATE TABLE IF NOT EXISTS `rasgosRaza` (
   `id_rasgo` int NOT NULL,
-  `id_raza` int NOT NULL
+  `id_raza` int NOT NULL,
+  KEY `fk_rasgoRaza-rasgo` (`id_rasgo`),
+  KEY `fk_rasgoRaza-raza` (`id_raza`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -381,7 +406,9 @@ CREATE TABLE IF NOT EXISTS `rasgosRaza` (
 DROP TABLE IF EXISTS `rasgosSubraza`;
 CREATE TABLE IF NOT EXISTS `rasgosSubraza` (
   `id_rasgo` int NOT NULL,
-  `id_subraza` int NOT NULL
+  `id_subraza` int NOT NULL,
+  KEY `fk_rasgo` (`id_rasgo`),
+  KEY `fk_subraza` (`id_subraza`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -393,7 +420,9 @@ CREATE TABLE IF NOT EXISTS `rasgosSubraza` (
 DROP TABLE IF EXISTS `rasgosTrasfondo`;
 CREATE TABLE IF NOT EXISTS `rasgosTrasfondo` (
   `id_rasgo` int NOT NULL,
-  `id_trasfondo` int NOT NULL
+  `id_trasfondo` int NOT NULL,
+  KEY `fk_rasgosTrasfondo-rasgo` (`id_rasgo`),
+  KEY `fk_rasgosTrasfondo-trasfondo` (`id_trasfondo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -404,8 +433,7 @@ CREATE TABLE IF NOT EXISTS `rasgosTrasfondo` (
 
 DROP TABLE IF EXISTS `razas`;
 CREATE TABLE IF NOT EXISTS `razas` (
-  `id` int NOT NULL,
-  `id_padre` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `descripcion` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `habilidades_puntos` json NOT NULL,
@@ -416,8 +444,7 @@ CREATE TABLE IF NOT EXISTS `razas` (
   `idiomas_elegir` json NOT NULL,
   `competencias_elegir` json NOT NULL,
   `espacios_de_conjuros` json NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `subraza` (`id_padre`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=ascii;
 
 -- --------------------------------------------------------
@@ -432,7 +459,8 @@ CREATE TABLE IF NOT EXISTS `subrazas` (
   `nombre` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `habilidades_puntos` json NOT NULL,
   `raza_padre` int NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `fk_padre` (`raza_padre`)
 ) ENGINE=InnoDB DEFAULT CHARSET=ascii;
 
 -- --------------------------------------------------------
@@ -472,17 +500,120 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` VALUES(1, 'test', 'test');
+INSERT INTO `usuarios` VALUES(1, 'test', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08');
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `razas`
+-- Filtros para la tabla `competenciasClase`
 --
-ALTER TABLE `razas`
-  ADD CONSTRAINT `subraza` FOREIGN KEY (`id_padre`) REFERENCES `razas` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `competenciasClase`
+  ADD CONSTRAINT `fk_competenciasClase-clase` FOREIGN KEY (`id_clase`) REFERENCES `clases` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `competenciasPersonaje`
+--
+ALTER TABLE `competenciasPersonaje`
+  ADD CONSTRAINT `fk_competenciasPersonaje-personaje` FOREIGN KEY (`id_personaje`) REFERENCES `personajes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `competenciasRaza`
+--
+ALTER TABLE `competenciasRaza`
+  ADD CONSTRAINT `fk_competenciasRaza-raza` FOREIGN KEY (`id_raza`) REFERENCES `razas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `competenciasTrasfondo`
+--
+ALTER TABLE `competenciasTrasfondo`
+  ADD CONSTRAINT `fk_competenciasTrasfondo-trasfondo` FOREIGN KEY (`id_trasfondo`) REFERENCES `trasfondos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `conjurosClase`
+--
+ALTER TABLE `conjurosClase`
+  ADD CONSTRAINT `fk_conjurosClase-clase` FOREIGN KEY (`id_clase`) REFERENCES `clases` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_conjurosClase-conjuro` FOREIGN KEY (`id_conjuro`) REFERENCES `conjuros` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `conjurosPersonaje`
+--
+ALTER TABLE `conjurosPersonaje`
+  ADD CONSTRAINT `fk_conjurosPersonaje-conjuro` FOREIGN KEY (`id_conjuro`) REFERENCES `conjuros` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_conjurosPersonaje-personaje` FOREIGN KEY (`id_personaje`) REFERENCES `personajes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `idiomasClase`
+--
+ALTER TABLE `idiomasClase`
+  ADD CONSTRAINT `fk_idiomasClase-clase` FOREIGN KEY (`id_clase`) REFERENCES `clases` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_idiomasClase-idioma` FOREIGN KEY (`id_idioma`) REFERENCES `idiomas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `idiomasPersonaje`
+--
+ALTER TABLE `idiomasPersonaje`
+  ADD CONSTRAINT `fk_idiomasPersonaje-idioma` FOREIGN KEY (`id_idioma`) REFERENCES `idiomas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_idiomasPersonaje-personaje` FOREIGN KEY (`id_personaje`) REFERENCES `personajes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `idiomasRaza`
+--
+ALTER TABLE `idiomasRaza`
+  ADD CONSTRAINT `fk_idiomaRaza-idioma` FOREIGN KEY (`id_idioma`) REFERENCES `idiomas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_idiomaRaza-raza` FOREIGN KEY (`id_raza`) REFERENCES `razas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `personajes`
+--
+ALTER TABLE `personajes`
+  ADD CONSTRAINT `fk_personajes-clase` FOREIGN KEY (`clase`) REFERENCES `clases` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_personajes-jugador` FOREIGN KEY (`jugador`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_personajes-raza` FOREIGN KEY (`raza`) REFERENCES `razas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_personajes-trasfondo` FOREIGN KEY (`trasfondo`) REFERENCES `trasfondos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `personajeUsuario`
+--
+ALTER TABLE `personajeUsuario`
+  ADD CONSTRAINT `fk_personajeUsuario-personaje` FOREIGN KEY (`id_personaje`) REFERENCES `personajes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_personajeUsuario-usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `rasgosClase`
+--
+ALTER TABLE `rasgosClase`
+  ADD CONSTRAINT `fk_rasgosClase-clase` FOREIGN KEY (`id_clase`) REFERENCES `clases` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_rasgosClase-rasgo` FOREIGN KEY (`id_rasgo`) REFERENCES `rasgos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `rasgosRaza`
+--
+ALTER TABLE `rasgosRaza`
+  ADD CONSTRAINT `fk_rasgoRaza-rasgo` FOREIGN KEY (`id_rasgo`) REFERENCES `rasgos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_rasgoRaza-raza` FOREIGN KEY (`id_raza`) REFERENCES `razas` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Filtros para la tabla `rasgosSubraza`
+--
+ALTER TABLE `rasgosSubraza`
+  ADD CONSTRAINT `fk_rasgo` FOREIGN KEY (`id_rasgo`) REFERENCES `rasgos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_subraza` FOREIGN KEY (`id_subraza`) REFERENCES `subrazas` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Filtros para la tabla `rasgosTrasfondo`
+--
+ALTER TABLE `rasgosTrasfondo`
+  ADD CONSTRAINT `fk_rasgosTrasfondo-rasgo` FOREIGN KEY (`id_rasgo`) REFERENCES `rasgos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_rasgosTrasfondo-trasfondo` FOREIGN KEY (`id_trasfondo`) REFERENCES `trasfondos` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Filtros para la tabla `subrazas`
+--
+ALTER TABLE `subrazas`
+  ADD CONSTRAINT `fk_padre` FOREIGN KEY (`raza_padre`) REFERENCES `razas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
