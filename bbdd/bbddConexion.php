cf7c30c1c1fa6ca4bd024d767c01ce8cb969513e
pvalls4 <valls.vilalta.4@gmail.com>
2021-12-01 18:49:15 +0100

    BBDD fixed

--- bbdd/bbddConexion.php
+++ bbdd/bbddConexion.php
@@ -3,8 +3,8 @@
 try {
   $hostname = "localhost";
   $dbname = "dragonesYmazmorras";
-  $username = "admin";
-  $pw = "admin";
+  $username = "manolo";
+  $pw = "manolo123";
   $pdo = new PDO ("mysql:host=$hostname;dbname=$dbname","$username","$pw");
 } catch (PDOException $e) {
   echo "Failed to get DB handle: " . $e->getMessage() . "\n";
