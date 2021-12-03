341f6511e82913e8eec010918ba99cc12d780094
MartiLlo <martillorachnaharro@gmail.com>
2021-12-03 16:24:12 +0100

    resumen de fichas del usuario

--- bbdd/bbddFunciones.php
+++ bbdd/bbddFunciones.php
@@ -26,4 +26,74 @@ function login($usuario, $contrasena, $oBbdd){
   unset($query);
 }
 
+function conseguirFichas($usuario, $oBbdd){
+
+  $queryUsr = $oBbdd->prepare("select id FROM usuarios where nombre= :nombre_jugador");
+  $queryUsr->bindParam(':nombre_jugador', $usuario);
+  $queryUsr->execute();
+
+  $eUsr= $queryUsr->errorInfo();
+  if ($eUsr[0]!='00000') {
+    echo "\nPDO::errorInfo():\n";
+    die("Error accedint a dades: " . $eUsr[2]);
+  } else {
+    $rowUsr = $queryUsr->fetch();
+    $id_usuario = $rowUsr["id"];
+
+    $queryPrs = $oBbdd->prepare("select * FROM personajes where jugador= :id_jugador");
+    $queryPrs->bindParam(':id_jugador', $id_usuario);
+    $queryPrs->execute();
+
+    $ePrs= $queryPrs->errorInfo();
+    if ($ePrs[0]!='00000') {
+      echo "\nPDO::errorInfo():\n";
+      die("Error accedint a dades: " . $ePrs[2]);
+    } else {
+      while ($rowPrs = $queryPrs->fetch()) {
+          $raza = conseguirNombreTabla($rowPrs["raza"], "razas", $oBbdd);
+          $clase = conseguirNombreTabla($rowPrs["clase"], "clases", $oBbdd);
+          printarResumenFicha($rowPrs["nombre"],$raza,$clase);
+      }
+    }
+
+    unset($queryUsr);
+    unset($queryPrs);
+  }
+}
+
+function printarResumenFicha($nombre, $raza, $clase){
+  echo "
+  <div class='resumenFicha'>
+    <div class='imgRaza'></div>
+    <p>$nombre</p>
+    <p>$raza</p>
+    <p>$clase</p>
+    <div class='resumenBotones'>
+      <button class='verFicha'><i class='far fa-eye'></i></button>
+      <button class='eliminarFicha'><i class='fas fa-trash-alt'></i></button>
+    </div>
+  </div>
+  ";
+}
+
+function conseguirNombreTabla($id, $tabla, $oBbdd){
+  $query = $oBbdd->prepare("select * FROM $tabla where id= :id");
+  $query->bindParam(':id', $id);
+  $query->execute();
+
+  $e= $query->errorInfo();
+  if ($e[0]!='00000') {
+    echo "\nPDO::errorInfo():\n";
+    die("Error accedint a dades: " . $e[2]);
+  }
+
+  $row = $query->fetch();
+  if($row){
+    return $row["nombre"];
+  }else{
+    trigger_error("no existe el id $id en la tabla $tabla");
+  }
+
+  unset($query);
+}
 ?>
---
+++ listaFichas.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<title>Tus Fichas</title>
+<?php include 'encabezado.php'; ?>
+  <body id="listaFichas">
+    <?php
+      include 'bbdd/bbddConexion.php';
+      include 'bbdd/bbddFunciones.php';
+      conseguirFichas($_SESSION["usuario"],$pdo); //la funcion esta hay q arreglarla
+    ?>
+  </body>
+</html>
