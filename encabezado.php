9b812c354a28384501b8b0b4640a38252303c36f
pvalls4 <valls.vilalta.4@gmail.com>
2021-12-16 19:15:14 +0100

    Añadidas Acces Keys completas v2

--- encabezado.php
+++ encabezado.php
@@ -20,7 +20,7 @@
       <div class="encabezadoDesplegable" id="encabezadoDesplegable">
         <a href="#" id="cierraSesion" accesskey="s">cierra sesión &nbsp<i class='fa fa-sign-out'></i></a>
       </div>
-      <button type="button" class="boton botonCasa" id="botonCasa" accesskey="t"><i class="fa fa-home"></i></button>
+      <button type="button" class="boton botonCasa" id="botonCasa" accesskey="h"><i class="fa fa-home"></i></button>
     <?php } else if (!$banderaInicio & !$banderaCrearCuenta){
       header('Location: '.'paginaInicio.php');
     }?>
