018236d804bf3116cac79febacd2637fa2586ef5
MartiLlo <martillorachnaharro@gmail.com>
2021-12-17 18:04:17 +0100

    stash

--- testearFicha.php
+++ testearFicha.php
@@ -6,16 +6,36 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script type="text/javascript" src="scripts/testearFicha.js"></script>
 	<title>Tablero</title>
-<?php include 'encabezado.php'; ?>
-	<div id="menuTestear">
-		<div class="fichaEnfrentada">
-			
+<?php include 'encabezado.php';
+	include 'bbdd/bbddConexion.php';
+	include 'bbdd/bbddFunciones.php';
+	if ($_POST['personaje']){
+		echo "
+		<div id='menuTestear'>
+			<div class='tuFicha'>
+				<img src='imagenes/razas/Semiorco.jpeg' alt='avatar'>
+				<h1>Juan</h1>
+				<personaje fuerza=12 destreza=12 constitucion=12 inteligencia=12 sabiduria=12 carisma=12 hidden>
+			</div>
+			<div class='botones'>
+				<button type='button' id='btnfuerza'>Fuerza</button>
+				<button type='button' id='btndestreza'>Destreza</button>
+				<button type='button' id='btnconstitucion'>Constitución</button>
+				<button type='button' id='btninteligencia'>Inteligencia</button>
+				<button type='button' id='btnsabiduria'>Sabiduría</button>
+				<button type='button' id='btncarisma'>Carisma</button>
+			</div>
+			<div class='fichaEnfrentada'>
+				<img src='imagenes/razas/Dummy.jpg' alt=avatar>
+				<h1>Muñeco de pruebas</h1>
+			</div>
 		</div>
-		
-		<div class="fichaEnfrentada">
+		";
+	} else {
+		conseguirFichasBis($_SESSION['usuario'], $pdo);
+	}
+?>
 
-		</div>
-	</div>
 	<?php include 'pie.php' ?>
 </body>
 </html>
